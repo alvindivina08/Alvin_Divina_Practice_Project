db4fc6fc641ba8d642ce0135c9e0ad20b9ed2550
alvindivina08 <alvindivina08@gmail.com>
2021-12-07 13:41:56 -0600

    example of a thread safe reporter.

--- SeleniumAutomation/src/main/java/com/testpractice/testcases/BaseClass.java
+++ SeleniumAutomation/src/main/java/com/testpractice/testcases/BaseClass.java
@@ -1,26 +1,46 @@
 package com.testpractice.testcases;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.testpractice.utilities.ExtentReport;
 import com.testpractice.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
+
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
+import org.testng.log4testng.Logger;
+
+import java.lang.reflect.Method;
+
 
 public class BaseClass {
     /*
      * @ThreadLocal Enables you to create variables that can only be read and written by the same thread
      */
-    ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
+    public ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
+    public static ThreadLocal<ExtentTest> extentTest = new ThreadLocal<>();
+    protected Logger logger = Logger.getLogger(BaseClass.class);
+    public ExtentReports extent;
+    public ExtentTest test;
     public String URL = "https://protect-us.mimecast.com/s/Dq2tCqx82YfMWNPOFZubbx?domain=way2automation.com";
     WebDriverFactory webDriverFactory;
 
+
+    @BeforeClass
+    public void setUp(){
+        extent = ExtentReport.extentReportGenerator();
+    }
 
     /*
      * @Parameters - enables you to pass parameters from testng.xml file to a java method.
      */
     @BeforeMethod
     @Parameters({"Browser"})
-    public void setUp(String Browser) throws InterruptedException {
+    public void setUp(String Browser, Method testMethod) throws InterruptedException {
+        test = extent.createTest(testMethod.getName());
+        extentTest.set(test);
         webDriverFactory = new WebDriverFactory();
         webDriverFactory.setDriver(Browser.toUpperCase());
         driver.set(webDriverFactory.getDriver());
@@ -32,6 +52,8 @@ public class BaseClass {
     @AfterMethod
     public void tearDown(){
         driver.get().close();
+        driver.get().quit();
+        extent.flush();
     }
 
 }
---
+++ SeleniumAutomation/src/main/java/com/testpractice/utilities/ExtentReport.java
@@ -0,0 +1,20 @@
+package com.testpractice.utilities;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+public class ExtentReport {
+    static ExtentReports extent;
+
+    public static ExtentReports extentReportGenerator(){
+        String path = System.getProperty("user.dir");
+        ExtentSparkReporter reporter = new ExtentSparkReporter(path + "/src/test/test-output/Reports/Spark");
+        extent = new ExtentReports();
+        extent.attachReporter(reporter);
+        reporter.config().setReportName("Personal Project");
+        reporter.config().setDocumentTitle("Test Results");
+        reporter.config().setTheme(Theme.DARK);
+        return extent;
+    }
+}
